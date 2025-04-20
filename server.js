1faec0f9719b0fc26cf73f9a9d652c5abbb88c87
mohammad nishad <mohammadnishad1243.mn@gmail.com>
2025-04-20 14:57:43 +0530

    solve msql bug

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.get('/', (req, res) => {
 // MySQL Connection
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
